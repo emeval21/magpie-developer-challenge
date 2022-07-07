e39a32d674f5ebc327e55f30679f3f00dcef6379
emeval21 <emeval21@yahoo.com>
2022-07-07 15:29:04 +0100

    first commit

--- src/Scrape.php
+++ src/Scrape.php
@@ -10,9 +10,22 @@ class Scrape
 
     public function run(): void
     {
-        $document = ScrapeHelper::fetchDocument('https://www.magpiehq.com/developer-challenge/smartphones');
+        $initial_page = ScrapeHelper::fetchDocument('https://www.magpiehq.com/developer-challenge/smartphones');
+        $page1 = ScrapeHelper::craw_page_products($initial_page);
+        $initial_page->filter('#pages div a')->each(function($link, $i){
+            $url = 'https://www.magpiehq.com/developer-challenge'.str_replace('..','',$link->attr('href'));
+            if($i !== 0){
+                $page_data = ScrapeHelper::fetchDocument($url);
+                $this->products = array_merge($this->products, ScrapeHelper::craw_page_products($page_data));
+            }
+        });
+        $this->products = array_merge($this->products, $page1);
+        $data = array_unique($this->products, SORT_REGULAR);
+        file_put_contents('output.json', str_replace('\\', '', json_encode($data)));
 
-        file_put_contents('output.json', json_encode($this->products));
+        // $document = ScrapeHelper::fetchDocument('https://www.magpiehq.com/developer-challenge/smartphones');
+
+        // file_put_contents('output.json', json_encode($this->products));
     }
 }
 
